cf41b984db441b24465a36634eb8a99775dac2e3
Adrian Hajdin - JavaScript Mastery <adrianhajdin00@gmail.com>
2021-08-19 10:43:44 +0200

    fix

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,4 +3,4 @@ import axios from 'axios';
 const url = 'http://localhost:5000/auth';
 
 export const signup = (userId) => axios.post(`${url}/signup`, { userId });
-export const login = (userId) => axios.post(url`${url}/login`, { userId });
+export const login = (userId) => axios.post(`${url}/login`, { userId });
