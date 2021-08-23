2e361d60f6b257a989289d8d8aeef7ca74c284f1
Adrian Hajdin - JavaScript Mastery <adrianhajdin00@gmail.com>
2021-08-23 14:23:55 +0200

    finalize new auth

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,20 +1,7 @@
 import React, { useState } from "react";
-// import { useDispatch } from 'react-redux';
-import {
-  CssBaseline,
-  Avatar,
-  Button,
-  Paper,
-  Grid,
-  Typography,
-  Container,
-} from "@material-ui/core";
-// import { useHistory } from 'react-router-dom';
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import axios from "axios";
-import useStyles from "./styles";
-import Input from "./Input";
 import Cookies from "universal-cookie";
+
 import signinImage from "../../assets/signup.jpg";
 import VisibilityIcon from "@material-ui/icons/Visibility";
 import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
@@ -29,9 +16,8 @@ const initialState = {
 const NewAuth = () => {
   const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(false);
-  const classes = useStyles();
-
   const [showPassword, setShowPassword] = useState(false);
+  
   const handleShowPassword = () => setShowPassword(!showPassword);
 
   const switchMode = () => {
@@ -106,35 +92,6 @@ const NewAuth = () => {
     setForm({ ...form, [e.target.name]: e.target.value });
 
   return (
-    // <Container component="main" maxWidth="xs">
-    // <CssBaseline />
-    //   <Paper className={classes.paper} elevation={6}>
-    //     <Avatar className={classes.avatar}>
-    //       <LockOutlinedIcon />
-    //     </Avatar>
-    //     <Typography component="h1" variant="h5">{ isSignup ? 'Sign up' : 'Sign in' }</Typography>
-    //     <form className={classes.form} onSubmit={handleSubmit}>
-    //       <Grid container spacing={2}>
-    //         { isSignup && <Input name="fullName" label="Full Name" handleChange={handleChange} autoFocus /> }
-    //         <Input name="username" label="Username" handleChange={handleChange} />
-    //         { isSignup && <Input name="phoneNumber" label="Phone Number" handleChange={handleChange} type="text" /> }
-    //         <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
-    //         { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
-    //       </Grid>
-    //       <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
-    //         { isSignup ? 'Sign Up' : 'Sign In' }
-    //       </Button>
-    //       <Grid container justifyContent="flex-end">
-    //         <Grid item>
-    //           <Button onClick={switchMode}>
-    //             { isSignup ? 'Already have an account? Sign in' : "Don't have an account? Sign Up" }
-    //           </Button>
-    //         </Grid>
-    //       </Grid>
-    //     </form>
-    //   </Paper>
-    // </Container>
-
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
@@ -146,7 +103,7 @@ const NewAuth = () => {
                 <input
                   name="fullName"
                   type="text"
-                  placeholder="John Doe"
+                  placeholder="Dr. Smith"
                   onChange={handleChange}
                   required
                 />
@@ -157,7 +114,7 @@ const NewAuth = () => {
               <input
                 name="username"
                 type="text"
-                placeholder="@john"
+                placeholder="drsmith"
                 onChange={handleChange}
                 required
               />
@@ -179,7 +136,7 @@ const NewAuth = () => {
               <input
                 name="password"
                 type={showPassword ? "text" : "password"}
-                placeholder="***"
+                placeholder="******"
                 onChange={handleChange}
                 required
               />
@@ -203,7 +160,7 @@ const NewAuth = () => {
                 <input
                   name="confirmPassword"
                   type={showPassword ? "text" : "password"}
-                  placeholder="***"
+                  placeholder="******"
                   onChange={handleChange}
                   required
                 />
