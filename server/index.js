d88406fb6cded0e2d8c0ec500e3050cd33f4d165
Adrian Hajdin - JavaScript Mastery <adrianhajdin00@gmail.com>
2021-08-25 20:34:05 +0200

    webhooks

--- server/index.js
+++ server/index.js
@@ -12,6 +12,31 @@ app.use(express.json())
 
 app.get('/', (_, res) => res.send('Hello World!'));
 
+app.post('/', (req, res) => {
+    let body = '';
+
+    req.on("data", (chunk) => {
+       body += chunk;
+    });
+ 
+    // Payload from Stream
+    req.on("end", () => {
+        let parsedBody = JSON.parse(body);
+
+        console.log({parsedBody});
+       
+        // Verifying there is a message in the payload
+        if(parsedBody.message === undefined) return;
+ 
+        // Here you can send an email, integrate an SMS service, or perform any other desired actions based on your Webhook events data
+        twilioClient.messages.create({ 
+            body: 'You have a new message.',  
+            messagingServiceSid: 'MG8893dd312da357e6883c026c93a2bb1a',      
+            to: '+385916114297' 
+        }).then(() => res.status(200).send("OK")).done();
+       });
+ });
+
 app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
